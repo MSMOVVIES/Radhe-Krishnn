879f4d7acf8d6b7ccb6acab6fe6ff5bba00ae8af
MSMOVVIES <142180881+MSMOVVIES@users.noreply.github.com>
2023-10-14 14:33:51 +0530

    commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -52,9 +52,13 @@ async def start(client, message):
                     InlineKeyboardButton('💰🤑 ᴇᴀʀɴ ᴍᴏɴᴇʏ 🤑💰', callback_data="shortlink_info")
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_chat_action(enums.ChatAction.TYPING)
+        m=await message.reply_sticker("CAACAgUAAxkBAAIFNGJSlfOErbkSeLt9SnOniU-58UUBAAKaAAPIlGQULGXh4VzvJWoeBA")
+        await asyncio.sleep(1)
+        await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            caption=START_MESSAGE.format(user=message.from_user.mention, bot=temp.B_LINK),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
